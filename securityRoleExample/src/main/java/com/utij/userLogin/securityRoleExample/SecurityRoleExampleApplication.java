1eba300b29bbb039211f0989c09b9f94ac86b275
JitenderPal <palljiten@gmail.com>
2018-06-14 01:40:34 +0530

    Ist Commit

---
+++ securityRoleExample/src/main/java/com/utij/userLogin/securityRoleExample/SecurityRoleExampleApplication.java
@@ -0,0 +1,12 @@
+package com.utij.userLogin.securityRoleExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityRoleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityRoleExampleApplication.class, args);
+	}
+}
---
+++ securityRoleExample/src/test/java/com/utij/userLogin/securityRoleExample/SecurityRoleExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.utij.userLogin.securityRoleExample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityRoleExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
